679cf442e9bb479d754a585063ec4a0b0399c683
YanNeu <yannic.neuhaus@student.uni-tuebingen.de>
2023-08-22 09:50:08 +0000

    update urls

--- setup_download.sh
+++ setup_download.sh
@@ -2,14 +2,14 @@
 
 # Download Spurious ImageNet
 cd dataset/spurious_imagenet
-wget 
+wget https://nc.mlcloud.uni-tuebingen.de/index.php/s/xBtTG9jnACJDxSg/download/images_100_classes.zip
 unzip images_100_classes.zip
 rm images_100_classes.zip
 cd ../..
 
 # Download precomputed alpha values (for SpuFix)
 cd neural_pca
-wget 
+wget https://nc.mlcloud.uni-tuebingen.de/index.php/s/4sfzFZcF3TKsWJ2/download/spurious_alphas_train.zip
 unzip spurious_alpha_train.zip
 rm spurious_alpha_train.zip
 cd ..
